879426e98b29b43b8d4924f6ecbf3d7020953dc4
FaizRA <faiz7835@students.amikom.ac.id>
2018-08-23 10:53:04 +0700

    search

---
+++ search.php
@@ -0,0 +1,102 @@
+<?php
+get_header();?>
+
+<!-- Projects Section -->
+<section id="projects" class="projects-section bg-light">
+  <div class="container">
+
+    <!-- Featured Project Row -->
+    <div class="container">
+      <div class="row">
+      <div class="col-xl-8 col-lg-7">
+        <h1 class="title">BLOG SEARCH</h1>
+
+
+
+        <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+          <br>
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+        <p><?php the_excerpt();
+        echo '<a href="' . esc_url( get_the_permalink() ) . '"> more...</a>'; ?></p>
+        <div id="postmeta">Publish on <?php the_time('F jS, Y'); ?> under <?php the_category(', '); ?> by <?php the_author(); ?> |
+        <?php comments_popup_link('No Comments &raquo;', '1 Comment &raquo;', '% Comments &raquo;'); ?>
+        <?php edit_post_link('Edit','','|'); ?></div>
+        </div>
+        <?php comments_template(); ?>
+        <hr>
+        <?php endwhile;?>
+<?php endif;?>
+
+      </div>
+    <!-- SIDEBAR -->
+
+<!--
+    <!-- Project One Row --+>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+      <div class="col-lg-6">
+        <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
+      </div>
+      <div class="col-lg-6">
+        <div class="bg-black text-center h-100 project">
+          <div class="d-flex h-100">
+            <div class="project-text w-100 my-auto text-center text-lg-left">
+              <h4 class="text-white">Misty</h4>
+              <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+              <hr class="d-none d-lg-block mb-0 ml-0">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Project Two Row --+>
+    <div class="row justify-content-center no-gutters">
+      <div class="col-lg-6">
+        <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
+      </div>
+      <div class="col-lg-6 order-lg-first">
+        <div class="bg-black text-center h-100 project">
+          <div class="d-flex h-100">
+            <div class="project-text w-100 my-auto text-center text-lg-right">
+              <h4 class="text-white">Mountains</h4>
+              <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+              <hr class="d-none d-lg-block mb-0 mr-0">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+
+
+<!-- Signup Section --+>
+<section id="signup" class="signup-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+        <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+
+        <form class="form-inline d-flex">
+          <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">
+          <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
+        </form>
+
+      </div>
+    </div>
+  </div>
+</section>
+-->
+
+
+
+<?php
+get_sidebar();
+get_footer();
