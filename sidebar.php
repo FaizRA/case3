50f08909319a04286ea081bc4cd6d6355283d945
FaizRA <faiz7835@students.amikom.ac.id>
2018-08-24 11:21:21 +0700

    update

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,28 @@
 <div class="col-xl-4 col-lg-5" style="padding-top:0px;margin-top:0px;">
   <div id="sidebar" class="featured-text text-center text-lg-left" style="padding-top:0px;margin-top:0px;">
+
+    <h4>Sample wp query</h4>
+    <?php
+    //cari kategori
+    $category_id = get_cat_ID('Bukan Saya');
+    $q = 'cat=' . $category_id;
+    query_posts($q);
+    if ( have_posts() ) : ?>
+    <ul>
+    <?php while ( have_posts() ) : the_post(); ?>
+       <!-- LOOP POST -->
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <li class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+
+    <?php endwhile;?>
+  </ul>
+    <?php else : ?>
+    <!-- No posts found -->
+    <?php endif;?>
+
+    <?php wp_reset_query(); ?>
+
+
     <h4>Menu [[PR]]</h4>
     <?php
     wp_nav_menu( array(
@@ -58,26 +81,7 @@ wp_nav_menu( array(
        <ul>
        <?php wp_list_pages('title_li='); ?>
      </ul>
-     <h4>Sample wp query</h4>
-     <?php
-     //cari kategori
-     $category_id = get_cat_ID('Bukan Saya');
-     $q = 'cat=' . $category_id;
-     query_posts($q);
-     if ( have_posts() ) : ?>
-     <ul>
-     <?php while ( have_posts() ) : the_post(); ?>
-        <!-- LOOP POST -->
-       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-       <li class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
 
-     <?php endwhile;?>
-   </ul>
-     <?php else : ?>
-     <!-- No posts found -->
-     <?php endif;?>
-
-     <?php wp_reset_query(); ?>
     </div>
 </div>
 
