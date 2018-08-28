dbad57dc501a0e6c209e5106e84975557c3c314d
FaizRA <faiz7835@students.amikom.ac.id>
2018-08-28 13:46:58 +0700

    jsonsync

--- functions.php
+++ functions.php
@@ -162,6 +162,35 @@ function codex_custom_init() {
 }
 add_action( 'init', 'codex_custom_init' );
 
+//jsonsync kalo pgn custon location
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 
+function my_acf_json_save_point( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/jsonsync';
+
+
+    // return
+    return $path;
+
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+
+    // remove original path (optional)
+    unset($paths[0]);
+
+
+    // append path
+    $paths[] = get_stylesheet_directory() . '/jsonsync';
+
+
+    // return
+    return $paths;
+
+}
 
 ?>
